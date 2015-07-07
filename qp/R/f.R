9e1226a8a19bdc83ae4c1032121e2ab5bef855d2
Laurent <lg390@cam.ac.uk>
2015-07-07 19:35:57 +0100

    package built

---
+++ qp/R/f.R
@@ -0,0 +1,6 @@
+##' The description of the function comes here.
+##'
+##' @title A friendly message
+##' @return \code{NULL}; used for its side effect.
+##' @author Laurent Gatto
+f <- function() message("Hello CambR")
